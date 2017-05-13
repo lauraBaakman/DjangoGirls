a1121acd202b89b8fc5fb71fa9585a68d28a1f93
Laura Baakman <laurabaakman@gmail.com>
2017-05-13 17:46:16 +0200

    Set up of the linter script.

--- .CI/linters.sh
+++ .CI/linters.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-echo "Hoi!";
+function lint_python {
+	echo "Python";
+}
+
+function lint_html {
+	echo "HTML";
+}
+
+function lint_js {
+	echo "Javascript";
+}
+
+function lint_css {
+	echo "CSS";
+}
+
+lint_python
+lint_html
+lint_js
+lint_css
