c3c88d4072bf6e067c4f0fc680b8e2e64ea7fe62
Laura Baakman <laurabaakman@gmail.com>
2017-05-16 21:52:27 +0200

    Show published posts in the view.

--- blog/models.py
+++ blog/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date = timezone.now()
